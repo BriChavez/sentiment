5bda8df1cffa637c19639ab384b3741294e0755f
BriChavez <brianachavez@gmail.com>
2022-09-16 16:29:53 -0700

    checkin out the tokenizers

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ classifier = pipeline("sentiment-analysis", model=model_name, tokenizer=tokenize
 
 results = classifier(["I love you", "i hate you"])
 # for result in results:
+
 #     print(result)
 
 tokens = tokenizer.tokenize("In this video I show you everything to get started with")
@@ -27,5 +28,14 @@ print(tokens)
 print(token_ids)
 print(token2_ids)
 
+# taining_data
+# pt is pytorch.
+# this will make sure all samples in our batch have the same length
+# pytoch tensor returned directly. explianition to come
+x_train = ["I love you", "i hate you"]
+batch = tokenizer(x_train, padding=True, truncation=True, max_length=True, return_tensors='pt')
 
-
+# disables the gradient tracking
+with torch.no_grad():
+    # ** means unpack
+    outputs = model(**batch)
